725a4a616fa9a5979fa5a324a20a0dc6108b31a3
Jacek Ardanowski <jacekard@gmail.com>
2018-12-08 10:29:10 +0100

    changed generator path to relative in proj and .gitignore

--- Generator/Generator/Generator.cs
+++ Generator/Generator/Generator.cs
@@ -4,6 +4,6 @@
 
     public static class Generator
     {
-        public static string Path = "C:/Users/Pawelb/Desktop/Programowanie/db/HD_Transportex/Generator/Generator/data/";
+        public static string Path = @"..\..\..\data\";
     }
 }
