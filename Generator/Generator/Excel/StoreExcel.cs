b0dfe55997cc51c3be3caa1299c951b92b8605a4
Jacek Ardanowski <jacekard@gmail.com>
2018-12-15 20:44:58 +0100

    Removed unnecessary fields from StoreExcel

--- Generator/Generator/Excel/StoreExcel.cs
+++ Generator/Generator/Excel/StoreExcel.cs
@@ -8,7 +8,7 @@
     {
         public static void Generate(int howMany)
         {
-            var header = "Adres;Kod Pocztowy;Pojemność [m^3];Godzina otwarcia;Godzina zamknięcia;Data rozpoczęcia działalności;Data zakończenia działalności";
+            var header = "Adres;Kod Pocztowy;Pojemność [m^3];Godzina otwarcia;Godzina zamknięcia";
             var name = "wyniki/StoreExcel.csv";
             var sep = ';';
 
@@ -31,13 +31,8 @@
                     var r1 = r.Next(0, address.Length);
                     var capacity = r.Next(2, 10) * 100;
 
-                    var startDate = date.Days();
-                    var closeDate = "Czynny";
-
                     writer.WriteLine(
-                        address[r1] + sep + capacity + sep + openTime[openRand] + sep + closeTime[closeRand] + sep
-                        + date.Days() + sep + closeDate
-                        );
+                        address[r1] + sep + capacity + sep + openTime[openRand] + sep + closeTime[closeRand]);
                 }
 
             }
